524750c30dc2046cacb7f2b80a7a095011ae6b08
Gastón Alejo Delucchi <gdelucchi@me.com>
2023-08-23 22:57:45 -0300

    Primer commit

--- src/app.js
+++ src/app.js
@@ -6,17 +6,22 @@ import usersRouter from './routes/users.router.js';
 import petsRouter from './routes/pets.router.js';
 import adoptionsRouter from './routes/adoption.router.js';
 import sessionsRouter from './routes/sessions.router.js';
+import cors from 'cors'
+import { port, mongoUrl } from '../process/config.js'
 
 const app = express();
-const PORT = process.env.PORT||8080;
-const connection = mongoose.connect(`URL DE MONGO`)
+const PORT = port || 8080;
+
+const connection = mongoose.connect(mongoUrl)
 
 app.use(express.json());
 app.use(cookieParser());
 
-app.use('/api/users',usersRouter);
-app.use('/api/pets',petsRouter);
-app.use('/api/adoptions',adoptionsRouter);
-app.use('/api/sessions',sessionsRouter);
+app.use(express.urlencoded({ extended: true }))
+app.use(cors())
+app.use('/api/users', usersRouter);
+app.use('/api/pets', petsRouter);
+app.use('/api/adoptions', adoptionsRouter);
+app.use('/api/sessions', sessionsRouter);
 
-app.listen(PORT,()=>console.log(`Listening on ${PORT}`))
+app.listen(PORT, () => console.log(`Listening on ${PORT}`))
